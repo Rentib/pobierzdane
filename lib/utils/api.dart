59d6ea77a53476d19cdcfccbb5826ba2ed2273f5
Rentib <sbitner420@tutanota.com>
2024-02-06 13:35:56 +0100

    fixed csv

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:csv/csv.dart';
-import 'package:sn_progress_dialog/options/cancel.dart';
-import 'package:sn_progress_dialog/options/completed.dart';
-import 'package:sn_progress_dialog/progress_dialog.dart';
+import 'package:sn_progress_dialog/sn_progress_dialog.dart';
 
 abstract class Api {
   static const int minYear = 2015;
@@ -28,7 +26,8 @@ abstract class Api {
   }
 
   final String _sort;
-  static const int _limit = 500; // stupid trezor api doesn't allow more than 500 records per request
+  static const int _limit =
+      500; // stupid trezor api doesn't allow more than 500 records per request
   static const String _format = 'csv';
 
   Api({
@@ -36,7 +35,11 @@ abstract class Api {
     required String name,
     required List<String> types,
     required String sort,
-  }) : _apiPath = apiPath, _name = name, _types = types, _sort = sort, _type = types.first;
+  })  : _apiPath = apiPath,
+        _name = name,
+        _types = types,
+        _sort = sort,
+        _type = types.first;
 
   Future<String> get({
     required BuildContext context,
@@ -44,44 +47,38 @@ abstract class Api {
     required int endYear,
   }) async {
     if (startYear < minYear || endYear > maxYear) {
-      throw ArgumentError('startYear must be >= $minYear and endYear must be <= $maxYear');
+      throw ArgumentError(
+          'startYear must be >= $minYear and endYear must be <= $maxYear');
     }
     if (startYear > endYear) {
       throw ArgumentError('startYear must be <= endYear');
     }
-
-    // TODO: change to List<Future<String>> to optimize sending requests
-    List<String> responses = [];
-
-    bool interrupted = false;
-    var client = http.Client();
     ProgressDialog pd = ProgressDialog(context: context);
+    var interrupted = false;
 
-    // FIXME: progress dialog is broken
     pd.show(
       max: 1,
-      msg: 'Przygotowywanie pobierania...',
-      progressType: ProgressType.valuable,
+      msg: 'Przygotowywanie danych...',
       cancel: Cancel(
         cancelClicked: () {
           interrupted = true;
-        }
+        },
       ),
     );
 
-    try {
-      int totalPages = 0;
+    int totalPages = 0;
+    Map<int, int> pagesInYear = {};
+    var client = http.Client();
 
-      Map<int, int> pagesInYear = {};
+    // TODO: move to separate function
+    try {
       for (int year = startYear; year <= endYear; ++year) {
         int low = 1, high = 1000000;
-
         while (low < high) {
           if (interrupted) {
-            throw Exception('Anulowano pobieranie danych');
+            throw Exception('Przerwano pobieranie danych');
           }
-
-          int mid = low + ((high - low) >> 1); // avoid overflow
+          int mid = low + ((high - low) >> 1);
           var response = await client.get(
             Uri.https(_baseUrl, _apiPath + _type, {
               'rok': year.toString(),
@@ -109,30 +106,41 @@ abstract class Api {
             high = mid;
           }
         }
-
         totalPages += low - 1;
         pagesInYear[year] = low - 1;
       }
-
+    } finally {
       pd.close();
-      pd.show(
-        max: totalPages,
-        msg: 'Pobieranie danych...',
-        progressType: ProgressType.valuable,
-        cancel: Cancel(
-          cancelClicked: () {
-            interrupted = true;
-          }
-        ),
-        completed: Completed(),
-      );
+      client.close();
+    }
 
+    if (totalPages == 0) {
+      throw Exception('Brak danych w wybranym zakresie');
+    }
+
+    client = http.Client();
+    pd.show(
+      max: totalPages,
+      msg: 'Pobieranie danych...',
+      cancel: Cancel(
+        cancelClicked: () {
+          interrupted = true;
+        },
+      ),
+      completed: Completed(
+        completedMsg: 'Pobieranie zakończone',
+      ),
+      progressType: ProgressType.valuable,
+    );
+
+    List<String> responses = [];
+    try {
+      int downloadedPages = 0;
       for (int year = startYear; year <= endYear; ++year) {
-        if (interrupted) {
-          throw Exception('Anulowano pobieranie danych');
-        }
-
         for (int page = 1; page <= pagesInYear[year]!; ++page) {
+          if (interrupted) {
+            throw Exception('Przerwano pobieranie danych');
+          }
           var response = await client.get(
             Uri.https(_baseUrl, _apiPath + _type, {
               'rok': year.toString(),
@@ -147,23 +155,38 @@ abstract class Api {
             await Future.delayed(const Duration(seconds: 5));
             --page;
             continue;
-          } else if (response.statusCode == 200) {
-            responses.add(response.body);
-            pd.update(value: responses.length);
           } else if (response.statusCode == 400) {
             throw Exception('Błąd 400: Nieprawidłowe parametry zapytania');
           } else if (response.statusCode == 404) {
             throw Exception('Błąd 404: Brak komunikacji z serwerem');
           } else if (response.statusCode == 500) {
             throw Exception('Błąd 500: Wewnętrzny błąd serwera');
+          } else if (response.statusCode != 200) {
+            throw Exception('Nieznany błąd');
+          }
+
+          responses.add(response.body);
+          downloadedPages++;
+          pd.update(value: downloadedPages);
+          if (!pd.isOpen()) {
+            pd.show(
+              max: totalPages,
+              msg: 'Pobieranie danych...',
+              cancel: Cancel(
+                cancelClicked: () {
+                  interrupted = true;
+                },
+              ),
+              completed: Completed(
+                completedMsg: 'Pobieranie zakończone',
+              ),
+              progressType: ProgressType.valuable,
+            );
           }
         }
       }
-    } catch (e) {
-      pd.close();
-      client.close();
-      rethrow;
     } finally {
+      pd.close();
       client.close();
     }
 
@@ -176,44 +199,63 @@ abstract class Api {
       csv += responses[i].split('\n').skip(2).join('\n');
     }
 
-    return const ListToCsvConverter(fieldDelimiter: ';').convert(const CsvToListConverter(fieldDelimiter: ',').convert(csv));
+    var rows = const CsvToListConverter(
+        fieldDelimiter: ',',
+        shouldParseNumbers: false,
+    ).convert(csv);
+    return const ListToCsvConverter(
+        fieldDelimiter: ';',
+    ).convert(rows);
   }
 }
 
 class PlanyFinansowe extends Api {
-  PlanyFinansowe() : super(
-    apiPath: 'api/${Api._apiVersion}/plany-finansowe-',
-    name: 'Plany Finansowe',
-    types: ['dochody', 'wydatki'],
-    sort: 'rok',
-  );
+  PlanyFinansowe()
+      : super(
+          apiPath: 'api/${Api._apiVersion}/plany-finansowe-',
+          name: 'Plany Finansowe',
+          types: ['dochody', 'wydatki'],
+          sort: 'rok',
+        );
 }
 
 class Sprawozdania extends Api {
-  Sprawozdania() : super(
-    apiPath: 'api/${Api._apiVersion}/sprawozdania-',
-    name: 'Sprawozdania',
-    types: ['RB50', 'RB28', 'RB27'],
-    sort: 'rok',
-  );
+  Sprawozdania()
+      : super(
+          apiPath: 'api/${Api._apiVersion}/sprawozdania-',
+          name: 'Sprawozdania',
+          types: ['RB50', 'RB28', 'RB27'],
+          sort: 'rok',
+        );
 }
 
 class UstawaBudzetowa extends Api {
-  UstawaBudzetowa() : super(
-    apiPath: 'api/${Api._apiVersion}/ustawa-budzetowa-',
-    name: 'Ustawa Budżetowa',
-    types: ['dochody', 'rezerwy', 'wydatki'],
-    sort: 'rok',
-  );
+  UstawaBudzetowa()
+      : super(
+          apiPath: 'api/${Api._apiVersion}/ustawa-budzetowa-',
+          name: 'Ustawa Budżetowa',
+          types: ['dochody', 'rezerwy', 'wydatki'],
+          sort: 'rok',
+        );
 }
 
 class Slownik extends Api {
-  Slownik() : super(
-    apiPath: 'api/${Api._apiVersion}/slownik/',
-    name: 'Słownik',
-    types: ['czesci', 'dysponenci', 'dzialy', 'grupy-ekonomiczne', 'jednostki-budzetowe', 'paragrafy', 'rozdzialy', 'zrodla-finansowania'],
-    sort: 'czesc,aktywny_od,aktywny_do',
-  );
+  Slownik()
+      : super(
+          apiPath: 'api/${Api._apiVersion}/slownik/',
+          name: 'Słownik',
+          types: [
+            'czesci',
+            'dysponenci',
+            'dzialy',
+            'grupy-ekonomiczne',
+            'jednostki-budzetowe',
+            'paragrafy',
+            'rozdzialy',
+            'zrodla-finansowania'
+          ],
+          sort: 'czesc,aktywny_od,aktywny_do',
+        );
 
   @override
   Future<String> get({
@@ -225,8 +267,9 @@ class Slownik extends Api {
 
     List<String> responses = [];
 
+    // TODO: make the same as in other get method
     try {
-      for (int page = 1; ; ++page) {
+      for (int page = 1;; ++page) {
         var response = await client.get(
           Uri.https(Api._baseUrl, _apiPath + type, {
             'format': Api._format,
@@ -259,17 +302,12 @@ class Slownik extends Api {
       csv += responses[i].split('\n').skip(2).join('\n');
     }
 
-    List<List<dynamic>> rows = const CsvToListConverter(fieldDelimiter: ',').convert(csv);
-
-    for (int i = 2; i < rows.length; ++i) {
-      var startDate = DateTime.parse(rows[i][2]);
-
-      if (startDate.year < startYear || startDate.year > endYear) {
-        rows.removeAt(i);
-        --i;
-      }
-    }
-
-    return const ListToCsvConverter(fieldDelimiter: ';').convert(rows);
+    var rows = const CsvToListConverter(
+        fieldDelimiter: ',',
+        shouldParseNumbers: false,
+    ).convert(csv);
+    return const ListToCsvConverter(
+        fieldDelimiter: ';',
+    ).convert(rows);
   }
 }
